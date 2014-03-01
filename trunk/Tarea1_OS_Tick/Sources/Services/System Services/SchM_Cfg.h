2cc8d58ebb17960d8b37a2f607e18e8f573a0bcd
sergio.astengo@gmail.com <sergio.astengo@gmail.com@be67c1cb-6952-a111-1936-8d207f63b12f>
2014-03-01 20:39:30 +0000

    OS Task Initial Release

--- trunk/Tarea1_OS_Tick/Sources/Services/System Services/SchM_Cfg.h
+++ trunk/Tarea1_OS_Tick/Sources/Services/System Services/SchM_Cfg.h
@@ -14,6 +14,10 @@
 *   03-01-14       02            SPA        OS Task Initial Release
 ******************************************************************************/
 
+/******************************************************************************
+*   Include Files
+******************************************************************************/
+#include "SchM_Types.h"
 
 /******************************************************************************
 *   Macro Definitions  
@@ -23,7 +27,27 @@
 /******************************************************************************
 *   Type Definitions
 ******************************************************************************/
+typedef enum
+{
+   TASK_1MS = 0,
+   TASK_4MS,
+   TASK_8MS,
+   TASK_16MS
+}SchM_TaskNumberType;
 
+typedef struct
+{
+  SchM_TaskNumberType SchM_TaskNumber;
+  u16 Mask;
+  u16 Offset;
+  void (*callback_function) (void);
+}SchM_Task_type;
+
+typedef struct
+{
+   SchM_TaskNumberType SchM_TaskNumber;
+   const SchM_Task_type *ptr_SchM_Task;  
+}SchM_TaskConfigType;
 /******************************************************************************
 *   External Variables
 ******************************************************************************/
