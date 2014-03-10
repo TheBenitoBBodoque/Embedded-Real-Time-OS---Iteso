8ca467520cd30d17a7bbe2863e062a09c5eac6c4
sergio.astengo@gmail.com <sergio.astengo@gmail.com@be67c1cb-6952-a111-1936-8d207f63b12f>
2014-03-10 05:53:33 +0000

    Scheduler First Beta

--- trunk/Tarea1_OS_Tick/Sources/Services/System Services/SchM_Cfg.h
+++ trunk/Tarea1_OS_Tick/Sources/Services/System Services/SchM_Cfg.h
@@ -18,11 +18,28 @@
 *   Include Files
 ******************************************************************************/
 #include "SchM_Types.h"
+#include "SchM_Tasks.h"
 
 /******************************************************************************
 *   Macro Definitions  
 ******************************************************************************/
 
+/* Task mask definition */
+#define MASK_1MS   ((u16)(0x0001)) /* 0000 0001 */
+#define MASK_4MS   ((u16)(0x0007)) /* 0000 0111 */
+#define MASK_8MS   ((u16)(0x000F)) /* 0000 1111 */
+#define MASK_16MS  ((u16)(0x001F)) /* 0001 1111 */
+#define MASK_32MS  ((u16)(0x003F)) /* 0011 1111 */
+#define MASK_64MS  ((u16)(0x004F)) /* 0111 1111 */
+
+/* Task mask offset */
+#define OFFSET_00  ((u16)(0))
+#define OFFSET_01  ((u16)(1))
+#define OFFSET_03  ((u16)(3))
+#define OFFSET_05  ((u16)(5))
+#define OFFSET_07  ((u16)(7))
+#define OFFSET_11  ((u16)(11))
+#define OFFSET_13  ((u16)(13))
 
 /******************************************************************************
 *   Type Definitions
@@ -38,20 +55,20 @@ typedef enum
 typedef struct
 {
   SchM_TaskNumberType SchM_TaskNumber;
-  u16 Mask;
-  u16 Offset;
-  void (*callback_function) (void);
+  SchM_TaskMaskType   Mask;
+  SchM_TaskOffsetType Offset;
+  void(*SchM_TaskCallback)(void) ;
 }SchM_Task_type;
 
 typedef struct
 {
-   SchM_TaskNumberType SchM_TaskNumber;
+   SchM_TaskNumberType SchM_TaskNumberConfig;
    const SchM_Task_type *ptr_SchM_Task;  
 }SchM_TaskConfigType;
 /******************************************************************************
 *   External Variables
 ******************************************************************************/
- 
+extern const SchM_TaskConfigType SchM_TaskConfigInitial[]; 
 /******************************************************************************
 *   Global Function Prototypes  
 ******************************************************************************/
